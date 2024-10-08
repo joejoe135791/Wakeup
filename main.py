f34b2b46892ba26997f8557e49c95a3f7020444d
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-10-07 22:24:55 -0700

    is it pretty? no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+from datetime import datetime
 import requests
 import feedparser
 import json
@@ -7,3 +8,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+currentDate = datetime.now()
+print("Sanity check: If you can read this, The first 12 lines of code are working")
+print()
+print("The current Date is", currentDate.month, currentDate.day, currentDate.year, "-", currentDate.hour,":", currentDate.minute)
