16fe7c4605583261cbf8805b6da91841a7555d2d
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2025-02-21 09:44:24 -0800

    what was I thinking when I made this lmao

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import time
 import datetime
 from datetime import datetime
 import requests
-import feedparser
+import threading
+import logging
 import json
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+sysLogger =logging.basicConfig()
+
 currentDate = datetime.now()
 print("Sanity check: If you can read this, The first 12 lines of code are working")
-print()
-print("The current Date is", currentDate.month, currentDate.day, currentDate.year, "-", currentDate.hour,":", currentDate.minute)
+print(f"The current Date is {currentDate.month}/{currentDate.day}/{currentDate.year}-{currentDate.hour}:{currentDate.minute}")
