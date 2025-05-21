cd59bc8be77ec68363b45ed15b5dcffdd8f64761
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2025-05-21 13:02:38 -0700

    more config stuffs

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import logging
 import json
 from dotenv import load_dotenv
 import os
+import toml
 
 load_dotenv()
-sysLogger =logging.basicConfig()
+with open('config.toml', 'r') as f:
+    config = toml.load(f)
+sysLogger = logging.basicConfig()
 
 currentDate = datetime.now()
 print("Sanity check: If you can read this, The first 12 lines of code are working")
 print(f"The current Date is {currentDate.month}/{currentDate.day}/{currentDate.year}-{currentDate.hour}:{currentDate.minute}")
+print("The script is ready!")
